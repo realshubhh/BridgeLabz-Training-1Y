4cf4f33df9a41cf5fbc5ebdfdbec61ab83270f17
realshubhh <shubhg274@gmail.com>
2026-01-17 14:09:21 +0530

    Primitive Data Types

---
+++ BasicPrograms/Primitive-Data-Type/Level1/FeeDiscountInput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class FeeDiscountInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fee = input.nextDouble();
+        double discountPercent = input.nextDouble();
+        double discountAmount = (fee * discountPercent) / 100;
+        double finalFee = fee - discountAmount;
+        System.out.println(
+            "The discount amount is INR " + discountAmount +
+            " and final discounted fee is INR " + finalFee
+        );
+    }
+}
