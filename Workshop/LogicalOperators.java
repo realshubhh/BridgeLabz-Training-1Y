5e9e97e694bff00a632cdfe523eccc22b3a8aab9
realshubhh <shubhg274@gmail.com>
2026-01-20 14:07:25 +0530

    Operators

---
+++ Workshop/LogicalOperators.java
@@ -0,0 +1,10 @@
+
+class LogicalOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a < b && b > 15);
+        System.out.println(a > b || b > 15);
+        System.out.println(!(a < b));
+    }
+}
---
+++ Workshop/UnaryOperators.java
@@ -0,0 +1,12 @@
+
+class UnaryOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(+a);
+        System.out.println(-a);
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+    }
+}
