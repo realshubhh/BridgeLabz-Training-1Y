a15bf898f5606e7839c4d6505a2db673a1982589
Shubh Goyal <shubhg274@gmail.com>
2026-02-09 14:17:33 +0530

    .

--- Workshop/TernaryOperator.java
+++
@@ -1,9 +0,0 @@
-
-class TernaryOperator {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int max = (a > b) ? a : b;
-        System.out.println("Maximum value is " + max);
-    }
-}
