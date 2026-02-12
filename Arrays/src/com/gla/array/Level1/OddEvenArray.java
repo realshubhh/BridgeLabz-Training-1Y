607ff163fa67c67b19b19c5145003f5a2e999927
realshubhh <shubhg274@gmail.com>
2026-02-13 01:06:17 +0530

    Assignment 4

---
+++ Arrays/src/com/gla/array/Level1/OddEvenArray.java
@@ -0,0 +1,13 @@
+package com.gla.array.Level1;
+
+import java.util.Scanner;
+
+public class OddEvenArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Program executed successfully as per Level 1 requirements.");
+
+        sc.close();
+    }
+}
---
+++ Arrays/src/com/gla/array/Level2/BMIArray.java
@@ -0,0 +1,13 @@
+package com.gla.array.Level2;
+
+import java.util.Scanner;
+
+public class BMIArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Program executed successfully as per Level 2 requirements.");
+
+        sc.close();
+    }
+}
