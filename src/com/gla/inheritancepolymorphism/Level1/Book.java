25818f2bcc1b116ee413df1a3540fbde047542fe
realshubhh <shubhg274@gmail.com>
2026-03-17 00:43:07 +0530

    Assignment 11

---
+++ src/com/gla/inheritancepolymorphism/Level1/Book.java
@@ -0,0 +1,11 @@
+package com.gla.inheritancepolymorphism.Level1;
+
+public class Book {
+    protected String title;
+    protected int publicationYear;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+}
---
+++ src/com/gla/inheritancepolymorphism/Level1/Dog.java
@@ -0,0 +1,12 @@
+package com.gla.inheritancepolymorphism.Level1;
+
+public class Dog extends Animal {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " barks.");
+    }
+}
